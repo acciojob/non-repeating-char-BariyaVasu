cec44f680a2291fb1fe0818f758f420462bd9cb6
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-18 00:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(str, input) {
  // Write your code here
+	const count = {};
+
+  // Count occurrences of each character
+  for (let char of str) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  // Find the first character with a count of 1
+  for (let char of str) {
+    if (count[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+	
 }
 const input = prompt("Enter a string");
+
 alert(firstNonRepeatedChar(input)); 
